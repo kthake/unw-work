d437ec7dac7c6fcb1c5df337f58f9654158b06a2
kthake <kthake@unwsp.edu>
2016-12-01 16:48:21 -0600

    Worked on Porjected Return Rate and get_fm_func

--- Retention_Tab/Projected_Return_Rate.sql
+++ Retention_Tab/Projected_Return_Rate.sql
@@ -1,34 +1,64 @@
-select  count(distinct z.PERSON_UID) "TOTAL",
-        count(distinct z1.PERSON_UID) "RETURNED",
-        count(distinct case when z.COMPLETION_IND = 'Y' and z1.PERSON_UID is null
-                            then null
-                            when z.STUDENT_TYPE = 'O' or z.PROGRAM = 'ADDTL'
-                            then null
-                            else z.PERSON_UID
-                       end) "RETURNABLE",
-        count(distinct case when z1.PERSON_UID is null and r.RETURN_RATING = 10
-                            then r.PERSON_UID
-                       end) * 0.1 "RED",                   
-        count(distinct case when z1.PERSON_UID is null and r.RETURN_RATING = 50
-                            then r.PERSON_UID
-                       end) * 0.5 "YELLOW",
-        count(distinct case when z1.PERSON_UID is null and r.RETURN_RATING = 90
-                            then r.PERSON_UID
-                       end) * 0.9 "GREEN"
-                       
-from ODSMGR.Z_ACADEMIC_STUDY_VW z
+select TYPE, 
+       VALUE,
+       ROWNUM "SORT"
+from
+  (
+  select round(RETURNED/RETURNABLE,4) * 100 "ACTUAL_RETURN_RATE",
+         round((RETURNED + RED + YELLOW + GREEN)/RETURNABLE,4) * 100 "PROJECTED_RETURN_RATE",
+         RED + YELLOW + GREEN "TOTAL_POTENTIAL_RETURNS",
+         RETURNED + RED + YELLOW + GREEN "PROJECTED_RETURNED",
+         a.*
+  from
+    (
+    select  --count(distinct z.PERSON_UID) "TOTAL",
+            count(distinct z1.PERSON_UID) "RETURNED",
+            count(distinct case when z.COMPLETION_IND = 'Y' and z1.PERSON_UID is null
+                                then null
+                                when z.STUDENT_TYPE = 'O' or z.PROGRAM = 'ADDTL'
+                                then null
+                                else z.PERSON_UID
+                           end) "RETURNABLE",
+            floor(count(distinct case when z1.PERSON_UID is null and r.RETURN_RATING = 10
+                                      then r.PERSON_UID
+                                 end) * 0.1 ) "RED",                   
+            floor(count(distinct case when z1.PERSON_UID is null and r.RETURN_RATING = 50
+                                      then r.PERSON_UID
+                                 end) * 0.5 ) "YELLOW",
+            floor(count(distinct case when z1.PERSON_UID is null and r.RETURN_RATING = 90
+                                      then r.PERSON_UID
+                                 end) * 0.9 ) "GREEN"
+                           
+    from ODSMGR.Z_ACADEMIC_STUDY_VW z
 
-      left outer join ODSMGR.Z_ACADEMIC_STUDY_VW z1
-      on z1.PERSON_UID = z.PERSON_UID
-      and z1.ACADEMIC_PERIOD_MOD = '20162'
-      and z1.PRIMARY_PROGRAM_IND = 'Y'
-      
-      left outer join NWC.Z_RETENTION_IND_TBL r
-      on r.PERSON_UID = z.PERSON_UID
-      and substr(r.ACADEMIC_PERIOD,1,5) = '20162'
-      
-where z.ACADEMIC_PERIOD_MOD = '20161'
-      and z.COLLEGE = 'TR'
-      and z.PRIMARY_PROGRAM_IND = 'Y'
-      
- 
+          left outer join ODSMGR.Z_ACADEMIC_STUDY_VW z1
+          on z1.PERSON_UID = z.PERSON_UID
+          and z1.ACADEMIC_PERIOD_MOD = '20162'
+          and z1.PRIMARY_PROGRAM_IND = 'Y'
+          
+          left outer join NWC.Z_RETENTION_IND_TBL r
+          on r.PERSON_UID = z.PERSON_UID
+          and substr(r.ACADEMIC_PERIOD,1,5) = '20162'
+          
+    where z.ACADEMIC_PERIOD_MOD = '20161'
+          and z.COLLEGE = 'TR'
+          and z.PRIMARY_PROGRAM_IND = 'Y'
+          
+    ) a
+  ) b
+unpivot (VALUE for type in (RETURNABLE as 'Returnable',
+                            RETURNED as 'Returned',
+                            PROJECTED_RETURNED as 'Projected Returned',
+                            ACTUAL_RETURN_RATE as 'Actual Return Rate',
+                            PROJECTED_RETURN_RATE as 'Porjected Return Rate',
+                            GREEN as 'Green', 
+                            YELLOW as 'Yellow', 
+                            RED as 'Red', 
+                            TOTAL_POTENTIAL_RETURNS as 'Total Potential Returns') )
+
+union 
+select '---------------------',
+       null,
+       5.5
+from dual
+
+order by 3
--- Z_GET_FM_FNC/get_fm_pidm_v3.sql
+++ Z_GET_FM_FNC/get_fm_pidm_v3.sql
@@ -6,21 +6,21 @@ Level 1 = president, level 2 = VP's, etc..
 Optionally pass a fiscal year when using i_type = 'POSN'
 otherwise defaults to current fiscal year.
 
-EXAMPLE: Z_GET_FM_FNC('POSN','P00223',2,null,'2016') to get VP over position P00223
-or Z_GET_FM_FNC('ORGN','2652',4,'N')
+EXAMPLE: Z_GET_FM_FNC('POSN',2,'P00223','2016') to get VP over position P00223
+or Z_GET_FM_FNC('ORGN',4,'2652','N') for the financial manager at level 4 over org 2652
 
 Error values and descriptions:
   -1 = POSN or ORGN not passed for i_type
   -2 = i_level is outside the range of 1-4
-  -3 = i_type = 'ORGN' but a COA was not passed for i_coa
+  -3 = i_type = 'POSN' but value passed for i_fsyr_coa is not length 4 (i.e. not the length of year format '2017')
+  -4 = i_type = 'ORGN' but value passed for i_fsyr_coa is not length 1 since COAs have only length of 1
 **********************************************/
 
-CREATE OR REPLACE FUNCTION Z_GET_FM_FNC_2 (  
+CREATE OR REPLACE FUNCTION Z_GET_FM_FNC (  
     i_type IN VARCHAR2, --pass 'POSN' if using a position and 'ORGN' if using an org
-    i_parm IN VARCHAR2, --pass a position or an org
     i_level IN SMALLINT,
-    i_coa IN VARCHAR2 DEFAULT NULL,
-    i_fsyr IN VARCHAR2 DEFAULT NULL ) 
+    i_parm IN VARCHAR2, --pass a position or an org
+    i_fsyr_coa IN VARCHAR2 DEFAULT NULL ) --pass fsyr when using i_type = 'POSN' and pass coa when using 'ORGN'
     
 RETURN NUMBER 
 
@@ -30,90 +30,71 @@ IS
     l_coa VARCHAR2(1);
     l_pidm NUMBER;
 
-CURSOR a IS
-  SELECT decode(i_level,'1',o.ORGANIZATION_LEVEL_1,
-                        '2',o.ORGANIZATION_LEVEL_2,
-                        '3',o.ORGANIZATION_LEVEL_3,
-                        '4',o.ORGANIZATION_LEVEL_4),
-         o.CHART_OF_ACCOUNTS
-  FROM (
-    SELECT POSITION_ORGANIZATION,
-           CHART_OF_ACCOUNTS,
-           PERCENTAGE,
-           row_number() over (order by PERCENTAGE desc, POSITION_ORGANIZATION) "ROW_NUM"
-    FROM ODSMGR.MPT_POSN_LABOR_DIST
-    WHERE FISCAL_YEAR = l_fsyr
-          and POSITION = i_parm ) p
-    inner join ODSMGR.MFT_ORGN_HIERARCHY o
-    on o.ORGANIZATION_CODE = p.POSITION_ORGANIZATION
-    and o.CHART_OF_ACCOUNTS = p.CHART_OF_ACCOUNTS
-    
-  WHERE ROW_NUM = 1;
-  
-  --internal function
-  FUNCTION GET_FM_PIDM_FNC(
-      p_org IN VARCHAR2,
-      p_coa IN VARCHAR2 )
-  RETURN NUMBER
-
-  IS
-    p_pidm NUMBER;
-    
-  BEGIN
-    SELECT DISTINCT o.FINANCIAL_MANAGER_UID
-    INTO p_pidm
-    FROM ODSMGR.MFT_ORGN_HIERARCHY o
-    WHERE o.ORGANIZATION_STATUS = 'A'
-          and o.ORGANIZATION_CODE = p_org
-          and o.CHART_OF_ACCOUNTS = p_coa;
-    RETURN p_pidm;
-  END GET_FM_PIDM_FNC;
-
 BEGIN
   --get current fiscal year if one is not passed as a parameter
-  SELECT NVL(i_fsyr,ATVFISC_CODE)
+  SELECT NVL(i_fsyr_coa,ATVFISC_CODE)
   INTO l_fsyr
   FROM ATVFISC
   WHERE sysdate between ATVFISC_START_DATE and ATVFISC_END_DATE;
-  
-  
-  
+   
   --error checking on values passed
   IF i_type not in ('POSN','ORGN')
      THEN l_pidm := -1;
      ELSIF i_level not between 1 and 4
      THEN l_pidm := -2;
-     ELSIF i_type = 'ORGN' and i_coa is null --possibly make this a check of i_coa against FTVCOAS
+     ELSIF i_type = 'POSN' and length(l_fsyr) <> 4
      THEN l_pidm := -3;
+     ELSIF i_type = 'ORGN' and length(l_fsyr) <> 1
+     THEN l_pidm := -4;
      ELSE
      --BEGIN POSN TYPE
-    IF i_type = 'POSN'
-      THEN                
-          OPEN a;
-          FETCH a INTO l_org, l_coa;
-          CLOSE a;
-    --END POSN TYPE
-    --BEGIN ORGN TYPE
-      ELSE  
-          SELECT DISTINCT decode(i_level,'1',o2.ORGANIZATION_LEVEL_1,
-                                         '2',o2.ORGANIZATION_LEVEL_2,
-                                         '3',o2.ORGANIZATION_LEVEL_3,
-                                         '4',o2.ORGANIZATION_LEVEL_4),
-                          o2.CHART_OF_ACCOUNTS
-          INTO l_org,
-               l_coa
-          FROM ODSMGR.MFT_ORGN_HIERARCHY o2            
-          WHERE o2.ORGANIZATION_STATUS = 'A'
-                and o2.CHART_OF_ACCOUNTS = i_coa
-                and o2.ORGANIZATION_CODE = i_parm;
-    --END ORGN TYPE
-    END IF;
+        IF i_type = 'POSN'
+          THEN                          
+              SELECT decode(i_level,'1',o.ORGANIZATION_LEVEL_1,
+                            '2',o.ORGANIZATION_LEVEL_2,
+                            '3',o.ORGANIZATION_LEVEL_3,
+                            '4',o.ORGANIZATION_LEVEL_4),
+                     o.CHART_OF_ACCOUNTS
+              INTO l_org, l_coa
+              FROM (
+                SELECT POSITION_ORGANIZATION,
+                       CHART_OF_ACCOUNTS,
+                       PERCENTAGE,
+                       row_number() over (order by PERCENTAGE desc, POSITION_ORGANIZATION) "ROW_NUM"
+                FROM ODSMGR.MPT_POSN_LABOR_DIST
+                WHERE FISCAL_YEAR = l_fsyr
+                      and POSITION = i_parm ) p
+                inner join ODSMGR.MFT_ORGN_HIERARCHY o
+                on o.ORGANIZATION_CODE = p.POSITION_ORGANIZATION
+                and o.CHART_OF_ACCOUNTS = p.CHART_OF_ACCOUNTS  
+              WHERE ROW_NUM = 1;
+        --END POSN TYPE
+        --BEGIN ORGN TYPE
+          ELSE  
+              SELECT DISTINCT decode(i_level,'1',o2.ORGANIZATION_LEVEL_1,
+                                             '2',o2.ORGANIZATION_LEVEL_2,
+                                             '3',o2.ORGANIZATION_LEVEL_3,
+                                             '4',o2.ORGANIZATION_LEVEL_4),
+                              o2.CHART_OF_ACCOUNTS
+              INTO l_org,
+                   l_coa
+              FROM ODSMGR.MFT_ORGN_HIERARCHY o2            
+              WHERE o2.ORGANIZATION_STATUS = 'A'
+                    and o2.CHART_OF_ACCOUNTS = i_fsyr_coa
+                    and o2.ORGANIZATION_CODE = i_parm;
+        --END ORGN TYPE
+        END IF;
     
-    l_pidm := GET_FM_PIDM_FNC(l_org,l_coa);
+    SELECT DISTINCT o.FINANCIAL_MANAGER_UID
+    INTO l_pidm
+    FROM ODSMGR.MFT_ORGN_HIERARCHY o
+    WHERE o.ORGANIZATION_STATUS = 'A'
+          and o.ORGANIZATION_CODE = l_org
+          and o.CHART_OF_ACCOUNTS = l_coa;
 
   END IF;
   
-  RETURN l_pidm;
+    RETURN l_pidm;
    
-END Z_GET_FM_FNC_2;
+END Z_GET_FM_FNC;
 
