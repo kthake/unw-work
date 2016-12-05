82fca07e6b4d87516c741ed1421e7a1c6a7e0915
kthake <kthake@unwsp.edu>
2016-12-05 16:53:41 -0600

    updates

--- Master_Student_Arrival_Tracker/Arrival_Tracker_v1.sql
+++ Master_Student_Arrival_Tracker/Arrival_Tracker_v1.sql
@@ -1,11 +1,9 @@
-select distinct
-       a1.PERSON_UID,
+select a1.PERSON_UID,
        a1.DATA_ORIGIN,
-       decode(substr(a1.DATA_ORIGIN,1,1),1,'Registered Trad',
-                                         2,'Registered PSOC',
-                                         3,'Deposited Potential PSOC',
-                                         4,'Deposited but not registered',
-                                         5,'Not returned') "DATA_ORIGIN_DESC",
+       decode(substr(a1.DATA_ORIGIN,1,1),1,'1-Registered Trad',
+                                         2,'2-Registered PSOC',
+                                         3,'3-Deposited Potential PSOC',
+                                         4,'4-Deposited Trad but not registered') "DATA_ORIGIN_DESC",
        a1.ID,
        a1.NAME_LFMI,
        a1.GENDER,
@@ -18,24 +16,25 @@ select distinct
        a1.BUILDING,
        a1.SECTION_ID,
        a1.ROOM,
-       a1.RD,
-       a1.RA_NAME,
+       nvl(NWC.Z_FORMAT_NAME_FNC(a1.RD_PIDM,'PL'),' ') "RD_NAME",
+       nvl(NWC.Z_FORMAT_NAME_FNC(a1.RA_PIDM,'PL'),' ') "RA_NAME",
        a1.RA_ROOM,
        a1.ARRIVAL_DOOR,
+       a1.CHECKED_IN_DATE,
        a1.STUDENT_TYPE,
        a1.COLLEGE
 from
 --------------------------------------------------------------------------START a1
   (
   select a.PERSON_UID,
-         listagg(a.DATA_ORIGIN,';') within group (order by a.DATA_ORIGIN asc) over (partition by a.PERSON_UID) "DATA_ORIGIN",
+         listagg(a.DATA_ORIGIN,'; ') within group (order by a.DATA_ORIGIN asc) over (partition by a.PERSON_UID) "DATA_ORIGIN",
          --RMS_ID
          MGKFUNC.F_GET_PERSON_INFO(a.PERSON_UID,'ID') "ID",
          p.FULL_NAME_LFMI "NAME_LFMI",
          p.GENDER,
          p.BIRTH_DATE,
-         substr(listagg(a.STUDENT_TYPE,';') within group (order by a.STUDENT_TYPE asc) over (partition by a.PERSON_UID),2,1) "STUDENT_TYPE",
-         substr(listagg(a.COLLEGE,';') within group (order by a.COLLEGE asc) over (partition by a.PERSON_UID),2,2) "COLLEGE",
+         substr(listagg(a.STUDENT_TYPE,'; ') within group (order by a.STUDENT_TYPE asc) over (partition by a.PERSON_UID),2,1) "STUDENT_TYPE",
+         substr(listagg(a.COLLEGE,'; ') within group (order by a.COLLEGE asc) over (partition by a.PERSON_UID),2,2) "COLLEGE",
          ODSMGR.Z_GET_EMAIL_FNC(a.PERSON_UID,'NWC') "UNW_EMAIL",
          NWC.Z_GET_CURRENT_PHONE_FNC(a.PERSON_UID,'MOBL') "CELL_PHONE",
          NWC.Z_GET_CONCAT_ADDR_UNWSP_PKG.Z_GET_CURR_ADDR_OF_TYPE_FNC(a.PERSON_UID,'PR') "PR_ADDRESS",
@@ -43,8 +42,8 @@ from
          nvl(MGKFUNC.F_GET_DESC('STVBLDG', r.BLDG_CODE),' ') "BUILDING",
          nvl(r.PARTITION,' ') "SECTION_ID",
          nvl(h.ROOM,' ') "ROOM", --add indication of DE?
-         nvl(NWC.Z_FORMAT_NAME_FNC(NWC.Z_GET_POSITION_INCUMBENT_FNC(b.RD_POSITION),'FL'),' ') "RD",
-         nvl(NWC.Z_FORMAT_NAME_FNC(act.PERSON_UID,'LFMI'), ' ') "RA_NAME",
+         NWC.Z_GET_POSITION_INCUMBENT_FNC(b.RD_POSITION) "RD_PIDM",
+         act.PERSON_UID "RA_PIDM",
          nvl(h1.ROOM,' ') "RA_ROOM",
          nvl(to_char(to_date(h.ARRIVAL_DATE, 'mm/dd/yyyy hh:mi:ss am'),'mm/dd/yyyy'), ' ') "CHECKED_IN_DATE",
          nvl(d.SLRRDEF_RDEF_CODE,' ') "ARRIVAL_DOOR",
@@ -53,13 +52,14 @@ from
                                    'OU','OUT',
                                    'WL','WAIT-LIST',
                                         'RESIDENT') "HOUSING_STATUS"
-         
-         
+
+
 
   from
 --------------------------------------------------------------------------START a
     (
     --GET STUDENTS REGISTERED FOR UPCOMING TERM
+
     select distinct z.PERSON_UID,
                     '1'||z.STUDENT_TYPE "STUDENT_TYPE",
                     '1'||z.COLLEGE "COLLEGE",
@@ -68,17 +68,16 @@ from
                          then 2
                          else 1
                     end "DATA_ORIGIN"
+
     from ODSMGR.Z_ACADEMIC_STUDY_VW z
-
-
-    where z.ACADEMIC_PERIOD_MOD = '20162'
-          and ( z.COLLEGE = 'TR'
-                or ( z.COLLEGE = 'DE' and z.TRAD_CREDITS > 0 ) )
-          and z.PRIMARY_PROGRAM_IND = 'Y'
+    where z.ACADEMIC_PERIOD_MOD = :ddTerm.MOD
           and z.WITHDRAWN_IND = 'N'
-       
+          and z.PRIMARY_PROGRAM_IND = 'Y'
+          and ( z.COLLEGE = 'TR'
+             or (z.COLLEGE = 'DE' and z.TRAD_CREDITS > 0) )
+
     union
-    
+
     --GET NEW STUDENTS THAT MAY NOT BE REGISTERED IN TRAD AND POTENTIAL PSOC STUDENTS
     select distinct aa.PERSON_UID,
                     '2'||aa.STUDENT_POPULATION,
@@ -86,58 +85,34 @@ from
                     substr(aa.ACADEMIC_PERIOD,1,5),
                     nvl2(att.PERSON_UID,3,4)
     from ODSMGR.ADMISSIONS_APPLICATION aa
-    
+
           left outer join ODSMGR.ADMISSIONS_ATTRIBUTE att
           on att.PERSON_UID = aa.PERSON_UID
           and att.ACADEMIC_PERIOD = aa.ACADEMIC_PERIOD
           and att.APPLICATION_NUMBER = aa.APPLICATION_NUMBER
           and att.ADMISSIONS_ATTRIBUTE = 'PSOC'
-          
-    where substr(aa.ACADEMIC_PERIOD,1,5) = '20162' --------------
+
+    where substr(aa.ACADEMIC_PERIOD,1,5) = :ddTerm.MOD
           and ( aa.COLLEGE = 'TR'
                 or (aa.COLLEGE = 'DE' and att.PERSON_UID is not null ) )
           and aa.LATEST_DECISION between '35' and '80'
           and substr(aa.LATEST_DECISION,1,1) <> '5'
-          
-    union
-    
-    select distinct z.PERSON_UID,
-                        '3'||z.STUDENT_TYPE "STUDENT_TYPE",
-                        '3'||z.COLLEGE "COLLEGE",
-                        z.ACADEMIC_PERIOD_MOD,
-                        5  "DATA_ORIGIN"
-    from ODSMGR.Z_ACADEMIC_STUDY_VW z
-    
-          left outer join ODSMGR.Z_ACADEMIC_STUDY_VW z1
-          on z1.PERSON_UID = z.PERSON_UID
-          and z1.ACADEMIC_PERIOD_MOD = '20162'
-
-
-    where z.ACADEMIC_PERIOD_MOD = '20161'
-          and z.COLLEGE = 'TR'
-          and z1.PERSON_UID is null
-          and z.PRIMARY_PROGRAM_IND = 'Y'
-          and z.WITHDRAWN_IND = 'N'
-          and ( z.COMPLETION_IND = 'N'
-                or z1.PERSON_UID is null )
-          and z.STUDENT_TYPE <> 'O'
-          and z.PROGRAM <> 'ADDTL'
     ) a
 --------------------------------------------------------------------------END a
 
     left outer join ODSMGR.Z_PERSON_NAME_VW p  --GENDER AND BIRTHDATE
     on p.PERSON_UID = a.PERSON_UID
-    
+
     left outer join ODSMGR.Z_RMS_HOUSING_VW h --RMS DATA
     on h.PERSON_UID = a.PERSON_UID
     and substr(h.BANNER_TERM_CODE,1,5) = a.ACADEMIC_PERIOD_MOD
-                    
+
                     left outer join ODSMGR.Z_ROOM_DEFINITION_VW r --BUILDING AND PARTITION/SECTION ID
                     on r.ROOM_NUMBER = h.ROOM
-                    
+
                                     left outer join ODSMGR.Z_BUILDING_VW b --RD POSITION CODE
                                     on b.BUILDING = r.BLDG_CODE
-                                    
+
                                     left outer join (ODSMGR.STUDENT_ACTIVITY act  --GET RA INFORMATION
                                                      inner join (ODSMGR.Z_RMS_HOUSING_VW h1
                                                                  inner join ODSMGR.Z_ROOM_DEFINITION_VW r1
@@ -151,6 +126,8 @@ from
                     left outer join SATURN.SLRRDEF d  --ARRIVAL DOOR
                     on d.SLRRDEF_ROOM_NUMBER = h.ROOM
                     and substr(MGKFUNC.F_GET_DESC('STVRDEF',d.SLRRDEF_RDEF_CODE),1,7) = 'Arrival'
-   
+
   ) a1
 --------------------------------------------------------------------------END a1
+
+where :bt_view is not null
--- Z_THE_VIEW/student_pop.sql
+++ Z_THE_VIEW/student_pop.sql
@@ -1,132 +1,41 @@
-with reg as (
-            select /*+ MATERIALIZE */  
-                  a.*,
-                  case when AUDIT_COUNT > 0 and REGISTERED_COUNT = 0
-                       then 'Y'
-                  end "AUDIT_ONLY_IND",
-                  case when REGISTERED_COUNT > 0
-                       then 'Y'
-                  end "REGISTERED_IND",
-                  case when WITHDRAWN_COUNT > 0 and AUDIT_COUNT + REGISTERED_COUNT = 0
-                       then 'Y'
-                  end "WITHDRAWN_IND",
-                  case when DROP_COUNT > 0 and AUDIT_COUNT + REGISTERED_COUNT + WITHDRAWN_COUNT = 0
-                       then 'Y'
-                  end "DROP_IND"
-            from
-              (
-              select distinct s.PERSON_UID,
-                              substr(s.ACADEMIC_PERIOD,1,5) "ACADEMIC_PERIOD_MOD",
-                              s.COURSE_REFERENCE_NUMBER,
-                              decode(s.REGISTRATION_STATUS,'AD','D', --DROP
-                                                           'AU','A', --AUDIT
-                                                           'AW','W', --WITHDRAWN
-                                                           'DC','D',
-                                                           'DD','D',
-                                                           'DW','D',
-                                                           'DX','D',
-                                                           'FX','W',
-                                                           'OD','D',
-                                                           'PP','NA', --UNSURE
-                                                           'PW','W',
-                                                           'PX','W',
-                                                           'RE','R', --REGISTERED
-                                                           'RW','R',
-                                                           'WC','W',
-                                                           'WL','W',
-                                                           'WP','NA',
-                                                           'WX','W') "REG_GROUP"
-                              
-              from MST_STUDENT_COURSE s
-              where s.TRANSFER_COURSE_IND = 'N'
-              )
-            pivot ( count(COURSE_REFERENCE_NUMBER) for  REG_GROUP in ('A' as AUDIT_COUNT,
-                                                                      'R' as REGISTERED_COUNT,
-                                                                      'W' as WITHDRAWN_COUNT,
-                                                                      'D' as DROP_COUNT,
-                                                                      'NA' as OTHERS_COUNT)
-                  ) a
-            ),
-            
-     adm as (
-            select /*+ MATERIALIZE */  
-                   a.*,
-                   to_date('01/01/2999','mm/dd/yyyy') "END_OF_TIME"
-            from
-              (
-              select distinct PERSON_UID,
-                              substr(ACADEMIC_PERIOD,1,5) "ACADEMIC_PERIOD_MOD",
-                              COLLEGE,
-                              DECISION_DATE,
-                              DECISION,
-                              SUBSTR(SOKODSF.F_SARAPPD_INFO(PERSON_UID, ACADEMIC_PERIOD, APPLICATION_NUMBER, 1, 'DECISION'), 1, 2) "LATEST_DECISION"
-                              
-              from ODSMGR.Z_ADMISSIONS_DECISION_TBL
-              where LATEST_APPLICATION_IND = 'Y'
-                )
-            pivot (min(DECISION_DATE) for DECISION in ('03' as D_03,
-                                                       '05' as D_05,
-                                                       '07' as D_07,
-                                                       '08' as D_08,
-                                                       '09' as D_09,
-                                                       '17' as D_17,
-                                                       '18' as D_18,
-                                                       '20' as D_20,
-                                                       '25' as D_25,
-                                                       '33' as D_33,
-                                                       '35' as D_35,
-                                                       '36' as D_36,
-                                                       '50' as D_50,
-                                                       '51' as D_51,
-                                                       '56' as D_56,
-                                                       '57' as D_57,
-                                                       '58' as D_58,
-                                                       '80' as D_80,
-                                                       '81' as D_81,
-                                                       '90' as D_90,
-                                                       '91' as D_91,
-                                                       '92' as D_92,
-                                                       '93' as D_93,
-                                                       '94' as D_94,
-                                                       '97' as D_97 )
-                  ) a
-            )
-
 select base.PERSON_UID,
-       p.ID,
-       base.ACADEMIC_PERIOD_MOD,
-       adm.COLLEGE,
-       --nvl2(reg.PERSON_UID,'REG','ADM') "DATA_ORIGIN",
-       p.GENDER,
-       p.BIRTH_DATE,
-       nullif(least(nvl(adm.D_25,END_OF_TIME),nvl(adm.D_33,END_OF_TIME)),END_OF_TIME) "MIN_ACCEPTED_DATE",
-       nullif(least(nvl(adm.D_35,END_OF_TIME),nvl(adm.D_36,END_OF_TIME)),END_OF_TIME) "MIN_CONFIRMED_DATE",
-       nullif(least(nvl(adm.D_50,END_OF_TIME),nvl(adm.D_51,END_OF_TIME),nvl(adm.D_56,END_OF_TIME),nvl(adm.D_57,END_OF_TIME),nvl(adm.D_58,END_OF_TIME)),END_OF_TIME) "MIN_DEFER_DATE",
-       nullif(least(nvl(adm.D_90,END_OF_TIME),nvl(adm.D_91,END_OF_TIME),nvl(adm.D_92,END_OF_TIME),nvl(adm.D_93,END_OF_TIME),nvl(adm.D_94,END_OF_TIME),nvl(adm.D_97,END_OF_TIME)),END_OF_TIME) "MIN_WITHDRAW_DATE"
+COUNT(BASE.PERSON_UID)
+--       p.ID,
+--       base.ACADEMIC_PERIOD_MOD,
+--       adm.COLLEGE,
+--       p.GENDER,
+--       p.BIRTH_DATE,
+--       nullif(least(nvl(adm.D25,END_OF_TIME),nvl(adm.D33,END_OF_TIME)),END_OF_TIME) "MIN_ACCEPTED_DATE",
+--       nullif(least(nvl(adm.D35,END_OF_TIME),nvl(adm.D36,END_OF_TIME)),END_OF_TIME) "MIN_CONFIRMED_DATE",
+--       nullif(least(nvl(adm.D50,END_OF_TIME),nvl(adm.D51,END_OF_TIME),nvl(adm.D56,END_OF_TIME),nvl(adm.D57,END_OF_TIME),nvl(adm.D58,END_OF_TIME)),END_OF_TIME) "MIN_DEFER_DATE",
+--       nullif(least(nvl(adm.D90,END_OF_TIME),nvl(adm.D91,END_OF_TIME),nvl(adm.D92,END_OF_TIME),nvl(adm.D93,END_OF_TIME),nvl(adm.D94,END_OF_TIME),nvl(adm.D97,END_OF_TIME)),END_OF_TIME) "MIN_WITHDRAW_DATE",
+--       reg.REGISTERED_IND
 
 from
   (
   select distinct PERSON_UID, ACADEMIC_PERIOD_MOD
-  from reg
+  from {{KTH_REG_POP_VW}} 
 
   union
   select distinct PERSON_UID, ACADEMIC_PERIOD_MOD
-  from adm
+  from {{KTH_ADM_POP_VW}} 
   ) base
   
   left outer join ODSMGR.Z_PERSON_NAME_VW p
   on p.PERSON_UID = base.PERSON_UID
   
-  left outer join reg
+  left outer join {{KTH_REG_POP_VW}} reg
   on reg.PERSON_UID = base.PERSON_UID
   and reg.ACADEMIC_PERIOD_MOD = base.ACADEMIC_PERIOD_MOD
   
-  left outer join adm
+  left outer join {{KTH_ADM_POP_VW}} adm
   on adm.PERSON_UID = base.PERSON_UID
   
 where base.ACADEMIC_PERIOD_MOD = '20161'
       and adm.ACADEMIC_PERIOD_MOD = 
             ( select max(ACADEMIC_PERIOD_MOD)
-              from adm adm1
+              from {{KTH_ADM_POP_VW}} adm1
               where adm1.PERSON_UID = base.PERSON_UID
                     and adm1.ACADEMIC_PERIOD_MOD <= base.ACADEMIC_PERIOD_MOD )
+                    
+                  GROUP BY BASE.PERSON_UID
